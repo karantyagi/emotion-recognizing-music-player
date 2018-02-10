a6999bd4dacbcffa535aeb22530dd7da42f216c5
kt21 <tyagi.k@husky.neu.edu>
2018-02-10 08:11:07 -0500

    adding music module

--- code/demo.py
+++ code/demo.py
@@ -51,15 +51,20 @@ def find_emotions(path):
         parsed = json.loads(response.text)
         # print (json.dumps(parsed, sort_keys=True, indent=2))
         frame_num = 1;
-
         for face in parsed:
             #print(' Frame {} :\t'.format(frame_num),face['faceAttributes']['emotion'])
             #print(' Smile {} :\t'.format(fno),face['faceAttributes']['smile'])
-            sum = (face['faceAttributes']['emotion']['happiness'] + face['faceAttributes']['emotion']['sadness']
-            + face['faceAttributes']['emotion']['neutral'] + face['faceAttributes']['emotion']['fear'] +
+            result =  (face['faceAttributes']['emotion']['sadness'] + face['faceAttributes']['emotion']['neutral'] + face['faceAttributes']['emotion']['fear'] +
             face['faceAttributes']['emotion']['disgust'] + face['faceAttributes']['emotion']['contempt'] +
             face['faceAttributes']['emotion']['anger'])
-            print('\t{}'.format(face['faceAttributes']['emotion']['happiness']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['surprise']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['neutral']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['sadness']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['disgust']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['anger']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['contempt']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['fear']).ljust(18)+'{}'.format(sum))
+
+            if result > face['faceAttributes']['emotion']['happiness']:
+                ans = 'sad'
+            else:
+                ans = 'happy'
+
+            # print(sum)
+            print('\t{}'.format(face['faceAttributes']['emotion']['happiness']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['surprise']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['neutral']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['sadness']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['disgust']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['anger']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['contempt']).ljust(18)+'{}'.format(face['faceAttributes']['emotion']['fear']).ljust(18)+"   "+ans)
             frame_num+=1
         # print(type(parsed[0]))
         #print(len(parsed))
@@ -77,7 +82,7 @@ def find_emotions(path):
 
 
 cv2.namedWindow("preview")
-vc = cv2.VideoCapture(1)
+vc = cv2.VideoCapture(0)
 
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
@@ -100,9 +105,9 @@ while rval:
     # start processing the image emotions
     if keyboard.is_pressed('p'):
         find_emotions(img_name)
-        key = cv2.waitKey() # milliseconds
+        key = cv2.waitKey(30) # milliseconds
     else:
-        key = cv2.waitKey() # milliseconds
+        key = cv2.waitKey(10) # milliseconds
     # take less frames
     # end processing this frame
 
@@ -111,10 +116,6 @@ while rval:
 
     # this can be put in a try catch box
     ## if file exists, delete it ##
-    if os.path.isfile(img_name):
-        os.remove(img_name)
-    else: ## Show an error ##
-        print("Error: %s file not found" % myfile)
     img_counter+=1
 
     if key == 27: # exit on ESC
@@ -122,6 +123,18 @@ while rval:
 
 cv2.destroyWindow("preview")
 vc.release()
+
+for i in range(1,img_counter):
+    ## if file exists, delete it ##
+    if os.path.isfile("img_frame{}.jpg".format(i)):
+        os.remove("img_frame{}.jpg".format(i))
+        # deleting the image after processing it
+        #print("Deleted img_frame{}.jpg".format(i))
+    else: ## Show an error ##
+        print("Error: %s file not found" % myfile)
+
+print("\n\n\tAll images deleted. Memory freed. Enjoy Lappy. ;)")
+
 
 
 
